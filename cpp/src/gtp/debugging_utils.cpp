ca3e1e001f3909a371bd55057609a91d66d752e6
Tancredi-Paul Grozav <paul@grozav.info>
2020-03-23 08:31:43 +0000

    Update debugging_utils.cpp

--- cpp/src/gtp/debugging_utils.cpp
+++ cpp/src/gtp/debugging_utils.cpp
@@ -1,22 +1,35 @@
 // Author: Tancredi-Paul Grozav <paul@grozav.info>
+// -------------------------------------------------------------------------- //
 // these are meant for debugging. Not optimized for speed.
+// -------------------------------------------------------------------------- //
+#include <string>
+#include <sstream>
+// -------------------------------------------------------------------------- //
+using namespace ::std;
+// -------------------------------------------------------------------------- //
 namespace gtp
 {
-  // convert string to list of ascii codes
-  auto str_to_u8_list = [&](const string &s) -> string
+  void debugging_utils__wrapper()
   {
-    const size_t s_size = s.size();
-    stringstream ss;
-    ss << "[";
-    for(size_t i=0; i<s_size; i++)
+// -------------------------------------------------------------------------- //
+    // convert string to list of ascii codes
+    auto str_to_u8_list = [&](const string &s) -> string
     {
-      ss << static_cast<unsigned int>(static_cast<unsigned char>(s.at(i)));
-      if(i+1 < s_size)
+      const size_t s_size = s.size();
+      stringstream ss;
+      ss << "[";
+      for(size_t i=0; i<s_size; i++)
       {
-        ss << ", ";
+        ss << static_cast<unsigned int>(static_cast<unsigned char>(s.at(i)));
+        if(i+1 < s_size)
+        {
+          ss << ", ";
+        }
       }
-    }
-    ss << "]";
-    return ss.str();
-  };
-}
+      ss << "]";
+      return ss.str();
+    };
+// -------------------------------------------------------------------------- //
+  }
+} // namespace gtp
+// -------------------------------------------------------------------------- //
